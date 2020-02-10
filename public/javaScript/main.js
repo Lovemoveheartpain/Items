d684eacb094b7c5a73df2d82faebedb2f9777694
Lovemoveheartpain <885140995@qq.com>
2020-02-10 15:43:52 +0800

    Update main.js

--- public/javaScript/main.js
+++ public/javaScript/main.js
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 console.log();
+=======
+// console.log();
+console.log('abcd');
+console.log('？？？')
+console.log('你好');
+console.log('全金鱼框架框架框架')
+console.log('框架')
+console.log('纳尼？')
+>>>>>>> parent of 9bb1d30... Update main.js
